1907f91a4a767ca957a4d24d1913d2b025872c80
Mariela <ruthmarielaorellana1998@gmail.com>
2020-07-14 21:11:42 -0600

    RestServer -usuario

--- server/config/config.js
+++ server/config/config.js
@@ -2,3 +2,25 @@
 //  Puerto
 // ============================
 process.env.PORT = process.env.PORT || 3000;
+
+
+// ============================
+//  Entorno
+// ============================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+// ============================
+//  Base de datos 
+// ============================
+
+
+let urlDB;
+
+//if(process.env.NODE_ENV === 'dev'){
+   // urlDB='mongodb://localhost:27017/cafe';
+//}else {
+    urlDB ='mongodb+srv://mariela21:N8yAuDjldBnhInZS@cluster0.y4cmm.mongodb.net/cafe';
+          // 'mongodb+srv://mariela21:<password>@cluster0.y4cmm.mongodb.net/test'
+//}
+
+process.env.URLDB = urlDB;
